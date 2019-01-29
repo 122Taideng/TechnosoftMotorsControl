fcd5d88dc69b77fd900305e5e665020c75443440
Lior Haim <Lior@dotcore.co.il>
2019-01-29 13:07:58 +0200

    Adding controller connection method
    Signed-off-by: Lior Haim <Lior@dotcore.co.il>

--- TechnosoftMotorsControl.cs
+++ TechnosoftMotorsControl.cs
@@ -5,5 +5,15 @@ namespace TechnsoftMotorsControl
 {
     public class MotorsControl
     {
+        private const string CHANNEL_NAME = "COM8";
+        private const uint BAUDRATE = 115200;
+        private const byte CHANNEL_TYPE = TMLLib.CHANNEL_RS232;
+
+        private byte host_id = 3;
+
+        public bool InitCommunicationChannel()
+        {
+            return (TMLLib.TS_OpenChannel(CHANNEL_NAME, CHANNEL_TYPE, host_id, BAUDRATE) < 0) ? false : true;
+        }
     }
 }
